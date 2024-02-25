d2ead42223529cedfb18a4b969833618c4f257bd
Phil Cooper <philjcooper@outlook.com>
2024-02-25 18:56:57 +0000

    added questions for manager role

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,6 +10,34 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+const team = []; // empty array to store added team members
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+async function addManager() {
+    const managerInfo = await inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the manager's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the manager's ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the manager's email address?"
+        },
+        {
+            type: "input",
+            name: "officeNum",
+            message: "What is the manager's office number?"
+        }
+    ]);
+    console.log(managerInfo);
+};
+
+addManager();
